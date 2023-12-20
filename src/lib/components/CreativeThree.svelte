8c1f72445dd8bc18d75fea995e107dce02c09961
TimOosterveer <62908209+TimOosterveer@users.noreply.github.com>
2023-12-20 14:27:37 +0100

    push

--- src/lib/components/CreativeThree.svelte
+++ src/lib/components/CreativeThree.svelte
@@ -1,5 +1,13 @@
 <div class="wrapper">
   <div class="loader">
+    <span style="--i:25"></span>
+    <span style="--i:24"></span>
+    <span style="--i:23"></span>
+    <span style="--i:22"></span>
+    <span style="--i:21"></span>
+    <span style="--i:20"></span>
+    <span style="--i:19"></span>
+    <span style="--i:18"></span>
     <span style="--i:17"></span>
     <span style="--i:16"></span>
     <span style="--i:15"></span>
@@ -23,16 +31,15 @@
 <style>
 
 .wrapper {
-  transform: translateX(-15rem) translateY(-5rem);
+  transform: translateY(-5rem);
 }
 
   .loader {
     position: relative;
     transform: skewY(-15deg) translateY(50px) translateX(50px);
     animation: animateColor 2.5s linear infinite;
-    transform: rotate(45deg); 
+    transform: rotate(80deg); 
   }
-
 
   .loader::before {
     content: '';
